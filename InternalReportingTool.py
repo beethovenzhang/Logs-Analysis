dd859eb9892e11f65b7e250db4054952201fdc04
Yunfei Zhang <beethovenzhang@gmail.com>
2018-11-01 23:05:15 -0700

    Add skeletons to source code

---
+++ InternalReportingTool.py
@@ -0,0 +1,20 @@
+# This is an internal reporting tool that answers the following
+# three questions about the news database:
+
+# 1. What are the most popular three articles of all time?
+# 2. Who are the most popular article authors of all time?
+# 3. On which days did more than 1% of requests lead to errors?
+
+import psycopg2
+
+def most_popular_articles():
+
+
+
+def most_popular_authors():
+
+
+def days_of_more_errors():
+
+
+if __name__ == '__main__':
