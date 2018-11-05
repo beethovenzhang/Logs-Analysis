b3e730bfd8864f54f938f8f5f2aa5c33a84282b5
Yunfei Zhang <beethovenzhang@gmail.com>
2018-11-04 21:35:04 -0800

    Add the function to answer the third question

--- InternalReportingTool.py
+++ InternalReportingTool.py
@@ -40,13 +40,26 @@ def most_popular_authors():
     print_results(res, 'views')
 
 
-#def days_of_more_errors():
+def days_of_more_errors():
+    '''Answers the third question about the days with more errors'''
+    db = psycopg2.connect(database=dbname)
+    c = db.cursor()
+    c.execute("""select error_count.date, CAST(errors as decimal(10))/CAST(normals as decimal(10)) as error_ratio
+    from error_count, normal_count
+    where error_count.date=normal_count.date
+    and CAST(errors as decimal(10))/CAST(normals as decimal(10))>0.01;""")
+    res = c.fetchall()
+    db.close()
+    print('The day with more than one percent errors:')
+    for row in res:
+        print(str(row[0]) + ' -- errors '+ str("{:.2%}".format(row[1])))
+
 
 
 def print_results(list, par):
     '''Prints out the query results in a clear format'''
     for row in list:
-        print(row[0] + ' -- ' + par + ' ' + str(row[1]))
+        print(str(row[0]) + ' -- ' + par + ' ' + str(row[1]))
 
 
 
@@ -55,3 +68,4 @@ if __name__ == '__main__':
     print()
     most_popular_authors()
     print()
+    days_of_more_errors()
